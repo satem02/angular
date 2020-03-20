c683c372277b6e39f0c6cae09fc0ea45706fe3b2
Şafak Temel <safaktemel02@gmail.com>
2020-03-20 09:01:52 +0300

    Add choice validator.

--- src/app/expansion-panel/views/new-authorization-form/others/user-approval-panel/user-approval-panel.component.ts
+++ src/app/expansion-panel/views/new-authorization-form/others/user-approval-panel/user-approval-panel.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit, forwardRef, Injector } from '@angular/core';
-import { NG_VALUE_ACCESSOR, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { NewAuthorizationBaseComponent } from '../../..';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 
@@ -13,10 +13,10 @@ import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => UserApprovalPanelComponent),
       multi: true
-    },{
+    }, {
       provide: MAT_RADIO_DEFAULT_OPTIONS,
       useValue: { color: 'primary' },
-  }
+    }
   ]
 })
 export class UserApprovalPanelComponent extends NewAuthorizationBaseComponent implements OnInit {
@@ -40,7 +40,7 @@ export class UserApprovalPanelComponent extends NewAuthorizationBaseComponent im
     this.form = this.formBuilder.group({
       NameSurname: new FormControl(),
       Description: new FormControl(),
-      ActionType: new FormControl(),
+      ActionType: new FormControl('', Validators.required),
       ActionDate: new FormControl(),
       Title: new FormControl()
     });
@@ -53,5 +53,20 @@ export class UserApprovalPanelComponent extends NewAuthorizationBaseComponent im
     this.form.valueChanges.subscribe(res => {
       this.responseData.emit();
     });
+  }
+
+  actionTypeChange(actionType: any) {
+    if (actionType.value == 3 || actionType.value == 2) {
+      const validators = [Validators.required, Validators.minLength(5)];
+      this.Description.setValidators(validators);
+    } else {
+      this.Description.clearValidators();
+    }
+    this.Description.updateValueAndValidity();
+  }
+
+
+  get Description() {
+    return this.form.get('Description') as FormControl;
   }
 }
